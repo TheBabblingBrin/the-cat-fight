b36a7bd5ba8ad874aebb2049f40cb8ba5383ace3
TheBabblingBrin <bhooverlee27@gmail.com>
2022-08-30 17:59:04 -0400

    initial

--- public/index.js
+++ public/index.js
@@ -1,1 +1,31 @@
-// Your code here
+// const pageLayout = () => {
+
+// }
+const titleLayout = async () =>{
+
+}
+const kittenPic = async () => {
+  const pic = await fetch("https://api.thecatapi.com/v1/images/search")
+  const url = await pic.json()
+  const img = document.createElement('img')
+  const div = document.createElement('div')
+  div.setAttribute('id', 'picture')
+  document.body.append(div)
+  img.src = url[0].url
+  const picDiv = document.getElementById('picture')
+  console.log(picDiv)
+  picDiv.append(img)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+window.onload = kittenPic, titleLayout
